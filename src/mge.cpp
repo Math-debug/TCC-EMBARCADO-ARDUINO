0c52c981c7b96ead2b5b697a0135bc3a837b3dab
Matheus <matheus.araujo.xavier@gmail.com>
2023-06-08 15:36:09 -0300

    resolvido DTO

--- src/mge.cpp
+++ src/mge.cpp
@@ -74,7 +74,7 @@ void loop() {
     Voltage voltage;
     String type;
     String toString() {
-      return String("{\"equipment\":{ \"id\":") + equipment.id + " }, \"current\": { \"current1\":" + current.current1 + ", \"current2\":"+ current.current2 +", \"current3\":"+ current.current3 +"  },\"voltage\":{ \"fase1fase2\":"+ voltage.fase1fase2 +", \"fase2fase3\":"+ voltage.fase2fase3 +", \"fase3fase1\":"+ voltage.fase3fase1 +"}, \"type\":\""+ tipoRede +"\"}";
+      return String("{\"equipment\":{\"id\":") + equipment.id + "},\"current\":{\"a\":" + current.current1 + ",\"b\":"+ current.current2 +",\"c\":"+ current.current3 +"},\"voltage\":{\"ab\":"+ voltage.fase1fase2 +",\"bc\":"+ voltage.fase2fase3 +",\"ca\":"+ voltage.fase3fase1 +"},\"type\":\""+ tipoRede +"\"}";
     }
   } keepAlive;
 
